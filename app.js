22cbb9d12e26e95f0741f8b7fa5705f9e8e7dd2b
MELEGHOST <kosbek043@gmail.com>
2025-02-10 05:46:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+// Функции для переключения экранов
+function showProfile() {
+  alert("Переход в профиль пользователя");
+  // Здесь можно добавить логику для перехода в профиль
+}
+
 function showMenu() {
   document.getElementById('welcome-screen').style.display = 'none';
   document.getElementById('menu-screen').style.display = 'block';
   document.getElementById('calendar-screen').style.display = 'none';
   document.getElementById('socials-screen').style.display = 'none';
   document.getElementById('films-screen').style.display = 'none';
+}
+
+function showCalendar() {
+  document.getElementById('menu-screen').style.display = 'none';
+  document.getElementById('calendar-screen').style.display = 'block';
+}
+
+function showSocials() {
+  document.getElementById('menu-screen').style.display = 'none';
+  document.getElementById('socials-screen').style.display = 'block';
+}
+
+function showFilms() {
+  document.getElementById('menu-screen').style.display = 'none';
+  document.getElementById('films-screen').style.display = 'block';
 }
