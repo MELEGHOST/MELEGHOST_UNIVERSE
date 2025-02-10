38af4e0778b1853c5b6aa20d2ede028088625c5d
MELEGHOST <kosbek043@gmail.com>
2025-02-10 05:42:03 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+.start-button {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, 40%); /* Опущена ниже центра */
+  width: 142.5px; /* Уменьшена на 5% */
+  height: auto;
+  cursor: pointer;
+  z-index: 10; /* Чтобы кнопка была поверх фрейма */
+}
