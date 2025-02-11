0ec1a5df8422fb4ae2ec157b58e514bfb62ce2fd
MELEGHOST <kosbek043@gmail.com>
2025-02-11 03:50:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// Функции для переключения экранов
-function showProfile() {
-  alert("Переход в профиль пользователя");
-  // Здесь можно добавить логику для перехода в профиль
-}
-
 document.addEventListener('DOMContentLoaded', function () {
   function showMenu() {
     console.log("Кнопка старта нажата!"); // Отладочное сообщение
@@ -13,19 +7,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Привязываем функцию к глобальному объекту window
   window.showMenu = showMenu;
+
+  function showProfile() {
+    alert("Переход в профиль пользователя");
+  }
+
+  function showCalendar() {
+    document.getElementById('menu-screen').style.display = 'none';
+    document.getElementById('calendar-screen').style.display = 'block';
+  }
+
+  function showSocials() {
+    document.getElementById('menu-screen').style.display = 'none';
+    document.getElementById('socials-screen').style.display = 'block';
+  }
+
+  function showFilms() {
+    document.getElementById('menu-screen').style.display = 'none';
+    document.getElementById('films-screen').style.display = 'block';
+  }
+
+  // Привязываем остальные функции к глобальному объекту window
+  window.showProfile = showProfile;
+  window.showCalendar = showCalendar;
+  window.showSocials = showSocials;
+  window.showFilms = showFilms;
 });
-
-function showCalendar() {
-  document.getElementById('menu-screen').style.display = 'none';
-  document.getElementById('calendar-screen').style.display = 'block';
-}
-
-function showSocials() {
-  document.getElementById('menu-screen').style.display = 'none';
-  document.getElementById('socials-screen').style.display = 'block';
-}
-
-function showFilms() {
-  document.getElementById('menu-screen').style.display = 'none';
-  document.getElementById('films-screen').style.display = 'block';
-}
