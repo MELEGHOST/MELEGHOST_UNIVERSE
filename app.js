81937e674d6fdc66a22e98000e39eba6714adace
MELEGHOST <kosbek043@gmail.com>
2025-02-11 03:40:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ function showProfile() {
 }
 
 function showMenu() {
+  console.log("Кнопка старта нажата!"); // Отладочное сообщение
   document.getElementById('welcome-screen').style.display = 'none';
   document.getElementById('menu-screen').style.display = 'block';
-  document.getElementById('calendar-screen').style.display = 'none';
-  document.getElementById('socials-screen').style.display = 'none';
-  document.getElementById('films-screen').style.display = 'none';
 }
 
 function showCalendar() {
